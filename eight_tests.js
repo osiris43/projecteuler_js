82d80f6552cd842ac299f08b40bc6e6180a2a48b
Brett Bim <bbim@onetechnologies.net>
2014-09-09 09:49:03 -0500

    eight

---
+++ eight_tests.js
@@ -0,0 +1,12 @@
+var should = require('should');
+var eight = require('./eight');
+var _ = require('underscore');
+
+describe('problem eight', function(){
+  it("should say the largest product of 4 numbers is 5832", function(){
+    eight.calculate(4).should.equal(5832);
+  })
+  it("should say the largest product of 13 numbers is 5832", function(){
+    eight.calculate(13).should.equal(5832);
+  })
+});
